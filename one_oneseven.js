ea2e52ffe7cfed554cc927879baf644f61deccc2
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-01-17 19:20:10 +0900

    Add files via upload

---
+++ one_oneseven.js
@@ -0,0 +1,11 @@
+//ES6의 가변 변수 사용 방법
+//let: 값을 수정할 수 있는 가변 변수를 위한 키워드
+
+let num = 1;
+num = num * 3;
+let str = '문자';
+str = '다른 문자';
+let arr = [];
+arr = [1,2,3];
+let obj = {};
+obj = { name: '새 객체' };

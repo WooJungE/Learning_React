bc68794c5d00983758540532cfc52afa7d9a3ec3
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-01-24 00:54:43 +0900

    Update one_twothree.js

--- one_twothree.js
+++ one_twothree.js
@@ -1,8 +1,13 @@
 const num1 = 1;
 const num2 = num1 * 3; // num2 = 3 
-const str1 = '¹®ÀÚ';
-const str2 = str1 + 'Ãß°¡'; //str2 = "¹®ÀÚÃß°¡"
-//const arr = [];
-//arr = [1, 2, 3]; 
-//const obj = {};
-//obj = { name: '³» ÀÌ¸§' };
+const str1 = 'ë¬¸ì';
+const str2 = str1 + 'ì¶”ê°€'; //str2 = "ë¬¸ìì¶”ê°€"
+const arr3 = [];
+const arr4 = arr3.concat(1); //arr4 = [1]
+const arr5 = [...arr4, 2, 3]; //arr5 = [1, 2, 3]
+const arr6 = arr5.slice(0, 1); // arr6 = [1], arr5 = [1, 2, 3]
+const [first, ...arr7] = arr5; //arr7 = [2, 3], first = 1
+const obj3 = { name: 'ë‚´ì´ë¦„', age: 20 };
+const objValue = { name: 'ìƒˆì´ë¦„', age: obj3.age };
+const obj4 = { ...obj3, name: 'ìƒˆì´ë¦„' }; // obj4 = { name: 'ìƒˆì´ë¦„', age: 20 }
+const { name, ...obj5 } = obj4; //obj5 = { age: 20 }

b1e15da31575967f3ff937b34dc5fa9e92ea43ac
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-01-23 10:40:34 +0900

    Add files via upload

---
+++ one_twothree.js
@@ -0,0 +1,8 @@
+const num1 = 1;
+const num2 = num1 * 3; // num2 = 3 
+const str1 = '문자';
+const str2 = str1 + '추가'; //str2 = "문자추가"
+//const arr = [];
+//arr = [1, 2, 3]; 
+//const obj = {};
+//obj = { name: '내 이름' };

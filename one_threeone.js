b0dc104eff66f32d7a2c8c57b6247ca621cbe428
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-02-02 10:00:31 +0900

    Update one_threeone.js

--- one_threeone.js
+++ one_threeone.js
@@ -6,11 +6,11 @@
 
 var x = 0;
 var y = 0;
-var obj = { x: x, y: y};
+var obj = { x: x, y: y}; //obj에 대입한 객체를 보면 키 이름이 키값과 동일(각각 x, y)함
 var randomKeyString = 'other';
 var combined = {};
-combined['one' + randomKeyString] = 'some value';
-var obj2 = {
+combined['one' + randomKeyString] = 'some value'; //연산 결과로 키값을 대입할 때는 키값을 지정할 코드를 추가로 작성해야 함
+var obj2 = { //객체에 함수를 추가할 때는 변수에 함수를 할당해야 함
   X: X,
   methodA: function() { console.log('method A'); },
   methodB: function() { return 0; },

077bbb70972f5b8d5986a95b7aae7b3980aac5a4
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-02-02 09:39:40 +0900

    Update one_threeone.js

--- one_threeone.js
+++ one_threeone.js
@@ -4,3 +4,14 @@
 //기존 자바스크립트의 객체 확장 표현식 사용 방법
 //객체와 객체의 값을 선언하기 위해 키 이름과 값을 각각 할당
 
+var x = 0;
+var y = 0;
+var obj = { x: x, y: y};
+var randomKeyString = 'other';
+var combined = {};
+combined['one' + randomKeyString] = 'some value';
+var obj2 = {
+  X: X,
+  methodA: function() { console.log('method A'); },
+  methodB: function() { return 0; },
+};

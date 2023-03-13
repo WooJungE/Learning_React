84cf4f2f2bbe2e72ae500e3ad0bb58d0f14b1b83
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-03-13 22:49:07 +0900

    Update three_onethree3.js

--- three_onethree3.js
+++ three_onethree3.js
@@ -1,1 +1,10 @@
-
+function parse(qs) {
+  const queryString = qs.substr(1); // querystring = 'banana=10&apple=20&orange=30'
+  const chunks = queryString.split('&'); // chunks = ['banana=10', 'apple=20', 'orange=30']
+  let result = {};
+  chunks.forEach((chunk) => {
+    const [ key, value ] = chunk.split('='); // key = 'banana', value = '10'
+    result[key] = value; // result = { banana: '10' }
+  });
+  return result;
+}

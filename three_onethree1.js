a56ceaf5a9b7e21e569bc6c752a5be2037fd73d2
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-03-13 22:27:09 +0900

    Update three_onethree1.js

--- three_onethree1.js
+++ three_onethree1.js
@@ -1,2 +1,14 @@
 //10, 20, 30을 문자열이 아닌 숫자로 변환하기
 
+function parse(qs) {
+  var queryString = qs.substr(1); // querystring = 'banana=10&apple=20&orange=30'
+  var chunks = qs.split('&');
+  var result = {};
+  for (var i = 0; i < chunks.length; i++) {
+    var parts = chunks[i].split('=');
+    var key = parts[0]; //key = 'banana'
+    var value = Number.isNaN(Number(parts[1])) ? parts[1] : Number(parts[1]);
+    result[key] = value; // result = { banana: '10' }
+  }
+  return result;
+}

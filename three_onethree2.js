7779057998f2da7b5cacdb493f320f8faef92387
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-03-13 22:44:32 +0900

    Update three_onethree2.js

--- three_onethree2.js
+++ three_onethree2.js
@@ -1,12 +1,12 @@
 function parse(qs) {
   var queryString = qs.substr(1); // querystring = 'banana=10&apple=20&orange=30'
-  var chunks = qs.split('&');
-  var result = {};
-  for (var i = 0; i < chunks.length; i++) {
-    var parts = chunks[i].split('=');
-    var key = parts[0]; //key = 'banana'
-    var value = parts[1]; //value = '10'
-    result[key] = value; // result = { banana: '10' }
-  }
+  const chunks = queryString.split('&'); // chunks = ['banana=10', 'apple=20', 'orange=30']
+  let result = {};
+  chunks.forEach((chunk) => {
+    const parts = chunks.split('='); //chunk = 'banana=10', parts = ['banana', '10']
+    const key = parts[0]; //key = 'banana'
+    const value = Number.isNaN(Number(parts[1])) ? parts[1] : Number(parts[1]);
+    result[key] = value; // result = { banana: 10 }
+  });
   return result;
 }

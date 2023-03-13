c0c473806b77b960fd0fc113ac0816435e3db2d0
WooJungE <114748876+WooJungE@users.noreply.github.com>
2023-03-13 22:33:01 +0900

    Create three_onethree2.js

---
+++ three_onethree2.js
@@ -0,0 +1,12 @@
+function parse(qs) {
+  var queryString = qs.substr(1); // querystring = 'banana=10&apple=20&orange=30'
+  var chunks = qs.split('&');
+  var result = {};
+  for (var i = 0; i < chunks.length; i++) {
+    var parts = chunks[i].split('=');
+    var key = parts[0]; //key = 'banana'
+    var value = parts[1]; //value = '10'
+    result[key] = value; // result = { banana: '10' }
+  }
+  return result;
+}
